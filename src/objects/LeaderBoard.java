14b287c34e2079607060a0a4df0fc572ce471b71
Diogo <diogo.tomas.teixeira@gmail.com>
2025-11-28 11:18:24 +0000

    sla

--- src/objects/LeaderBoard.java
+++ src/objects/LeaderBoard.java
@@ -1,5 +1,12 @@
 package objects;
 
+import java.util.Map;
+
 public class LeaderBoard {
 
+  private Map<String,Integer> bestScores;
+
+  public Map<String, Integer> getBestScores() {
+    return bestScores;
+  }
 }
